0dc9a5aee7c4405c2a3b64cc2465a598d7e8b9c0
JPPPPPPPPP <joaopargana@gmail.com>
2024-05-01 00:54:07 +0100

    i hate performance metrics

--- lab1/mm1.py
+++ lab1/mm1.py
@@ -30,11 +30,6 @@ def simulate_mm1(arrival_rate, service_rate, simulation_time):
   while(event_list):
     # Read tuple from event list
     current_event = event_list.pop(0)
-    # Update current time
-    current_time = current_event[0]
-    # Check if simulation time has been reached
-    if(current_time > simulation_time):
-      break
     # Check for type of event
     if(current_event[1] == "arrival"):
       # If arrival, add it to queue
@@ -42,30 +37,38 @@ def simulate_mm1(arrival_rate, service_rate, simulation_time):
       # Generate a new packet arrival event
       next_event = (current_event[0] + generate_events(1, arrival_rate)[0], "arrival")
       event_list.append(next_event)
-      #If the server is free and the queue is not empty, schedule a departure event
-      if(not server_busy and not event_queue):
-        continue
-      else:
-        # Take oldest packet from queue
-        current_departure = event_queue.pop(-1)
-        # Mark server as busy
-        server_busy = True
-        # Add departure event to list
-        event_list.append((next_event[0] + generate_events(1, service_rate)[0], "departure"))
     else:
       # If departure, mark server as free
       server_busy = False
-      # If the server is free and the queue is not empty, schedule a departure event
-      if(not server_busy and not event_queue):
-        continue
-      else:
-        # Take oldest packet from queue
-        current_departure = event_queue.pop(-1)
-        # Mark server as busy
-        server_busy = True
-        # Add departure event to list
-        event_list.append((current_departure[0] + generate_events(1, service_rate)[0], "departure"))
-  
+      # Update waiting times
+      total_waiting_time += current_event[0] - current_time
+      current_time = current_event[0]
+    # If the server is free and the queue is not empty, schedule a departure event
+    if(not server_busy and not event_queue):
+      # Update system idle time
+      system_idle_time += current_event[0] - current_time
+      #print(current_event[0], current_time)
+      current_time = current_event[0]
+      continue
+    # Take oldest packet from queue
+    current_event = event_queue.pop(0)
+    # Mark server as busy
+    server_busy = True
+    # Add departure event to list
+    event_list.append((current_event[0] + generate_events(1, service_rate)[0], "departure"))
+    # Update number of customers
+    num_customers += 1
+    # Update waiting times
+    total_queue_waiting_time += current_event[0] - current_time
+    total_waiting_time += current_event[0] - current_time
+    # Update current time
+    current_time = current_event[0]
+    # Sort event list by time
+    event_list = sorted(event_list, key=lambda x: x[0])
+    # Check if simulation time has been reached
+    if(current_time > simulation_time):
+      break
+
   # Calculate performance metrics after loop termination
   average_waiting_time = total_waiting_time / num_customers if num_customers > 0 else 0
   average_time_in_queue = total_queue_waiting_time / num_customers if num_customers > 0 else 0
@@ -78,7 +81,7 @@ def simulate_mm1(arrival_rate, service_rate, simulation_time):
   }
 
 # Example usage
-results = simulate_mm1(arrival_rate=0.5, service_rate=0.25, simulation_time=10)
+results = simulate_mm1(arrival_rate=2, service_rate=3, simulation_time=50)
 print("Average waiting time:", results["average_waiting_time"])
 print("Average time in queue:", results["average_time_in_queue"])
 print("System utilization:", results["system_utilization"])
